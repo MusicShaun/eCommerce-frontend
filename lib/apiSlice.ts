92896f5ca32c20928655703c33850d22bd9bc802
MusicShaun <music.shaunpickett@gmail.com>
2023-05-19 07:23:36 +0800

    wrong api link oops

--- lib/apiSlice.ts
+++ lib/apiSlice.ts
@@ -24,7 +24,7 @@ interface Status {
 export const apiSlice = createApi({
   reducerPath: 'apiSlice',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shaunysshop.com/',
+    baseUrl: 'https://shauns-ecommerce.herokuapp.com/api/asos/',
     prepareHeaders: (headers, { getState }) => {
       // Get the JWT token from your Redux store state or any suitable storage mechanism
       const token = (getState() as RootState).auth.key;

2cecfb144c2a1746c9399f2c8c9f3be167483399
MusicShaun <music.shaunpickett@gmail.com>
2023-05-27 14:13:53 +0800

    AWS updated and deployed

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "http://localhost:3000/,https://www.shaunysshop.com/",
+        "redirectSignOut": "http://localhost:3000/,https://www.shaunysshop.com/login",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
